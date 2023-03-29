4e351e9c5fc0cbba5169b4c1f6e58257c8a26e75
asmraihan <asmraihanctg14@gmail.com>
2023-03-30 00:49:38 +0600

    email added

--- src/components/SideCart/SideCart.jsx
+++ src/components/SideCart/SideCart.jsx
@@ -26,15 +26,15 @@ const SideCart = ({time}) => {
             <h3 className='text-2xl font-semibold py-5'>Side cart</h3>
             <div className='sticky top-10 border-2 bg-blue-300 p-5 rounded-md'>
             <p>Total Watch Time : </p>
-            <p className='border-2 rounded-md my-2 bg-red-300 p-2'>{updatedTime}</p>
+            <p className='border-2 rounded-md my-2 bg-red-300 p-2'>{updatedTime} min</p>
             <p className='my-2'>Add break time</p>
             <div>
                 <button onClick={()=>handelBreakTime(10)} className='focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800'>10</button>
                 <button className='focus:outline-none text-white bg-yellow-400 hover:bg-yellow-500 focus:ring-4 focus:ring-yellow-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:focus:ring-yellow-900">Yellow'>20</button>
                 <button className='focus:outline-none text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-5 py-2.5 mb-2 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900'>30</button>
             </div>
-            <p className='border-2 rounded-md my-2 bg-red-300 p-2'>{breakTime}</p>
-            <button onClick={()=>toast("Wow so easy!")}
+            <p className='border-2 rounded-md my-2 bg-red-300 p-2'>{breakTime} min</p>
+            <button onClick={()=>toast(`${updatedTime - breakTime} min`)}
         href="#"
         className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-3 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
       >
